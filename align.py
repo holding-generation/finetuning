0ad004139a32c81bd10b01196b166ab2b4fff6c9
jesse-woo <114460358+jesse-woo@users.noreply.github.com>
2024-04-12 22:23:43 -0400

    freelaw scraping script

--- align.py
+++ align.py
@@ -3,6 +3,7 @@ import nltk
 
 nltk.download('punkt')
 
+print("Score the model")
 scorer = AlignScore(model='roberta-base', batch_size=32, device='cuda:0', ckpt_path='./AlignScore-base.ckpt', evaluation_mode='nli_sp')
 score = scorer.score(contexts=['hello world.'], claims=['hello world.'])
 
